8bc291a05c26520e3ae1cf16c20fa075b3e03b9b
syzkii <rifkiardianakbar.raa@gmail.com>
2021-03-22 20:42:29 +0700

    updated

--- login.php
+++ login.php
@@ -37,7 +37,7 @@
         </div>
         <div class="bg-blue py-4">
             <div class="row px-3"> 
-            <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; 2021. All rights reserved by Rifki Ardian.</small>
+            <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; 2021. All rights reserved by <a href="#">Rifki Ardian.</small></a>
             </div>
         </div>
     </div>
